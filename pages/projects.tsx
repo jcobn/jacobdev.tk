889f69e444becbd68128bda58881aa3d3080dc94
Kubajsa <71178261+Kubajsa@users.noreply.github.com>
2021-05-03 18:44:10 +0200

    Fix viewing projects page on phones

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,59 +4,61 @@ import styles from "./Projects.module.scss";
 
 export default function Projects() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Kubajsa | Jacob - Projects</title>
-      </Head>
-      <h2>
-        <Typist>my projects</Typist>
-      </h2>
-      <p>
-        <a
-          href="https://github.com/Kubajsa/randomTp"
-          className={styles.project}
-          target="_blank"
-        >
-          RandomTP
-        </a>{" "}
-        - Minecraft Spigot 1.13-1.16 RandomTP plugin{" "}
-        <span className={styles.language}>(Java)</span>
-      </p>
-      <p>
-        <a
-          href="https://quiz.jacobdev.tk"
-          className={styles.project}
-          target="_blank"
-        >
-          React Quiz App
-        </a>{" "}
-        - Simple Quiz App made with ReactJS{" "}
-        <span className={styles.language}>(ReactJS, TypeScript)</span>
-      </p>
-      <p>
-        <a
-          href="https://github.com/Kubajsa/jacobdev.tk"
-          className={styles.project}
-          target="_blank"
-        >
-          jacobdev.tk
-        </a>{" "}
-        - My First Website <span className={styles.language}>(NextJS)</span>
-      </p>
-      <p>
-        <a
-          href="https://github.com/Kubajsa/discord.pyBot"
-          className={styles.project}
-          target="_blank"
-        >
-          Kubajsa's Bot
-        </a>{" "}
-        - Discord bot made with discord.py{" "}
-        <span className={styles.language}>(Python)</span>
-      </p>
-      <a href="https://jacobdev.tk" className={styles.homeButton}>
-        home
-      </a>
+    <div className={styles.flex}>
+      <div className={styles.container}>
+        <Head>
+          <title>Kubajsa | Jacob - Projects</title>
+        </Head>
+        <h2>
+          <Typist>my projects</Typist>
+        </h2>
+        <p>
+          <a
+            href="https://github.com/Kubajsa/randomTp"
+            className={styles.project}
+            target="_blank"
+          >
+            RandomTP
+          </a>{" "}
+          - Minecraft Spigot 1.13-1.16 RandomTP plugin{" "}
+          <span className={styles.language}>(Java)</span>
+        </p>
+        <p>
+          <a
+            href="https://quiz.jacobdev.tk"
+            className={styles.project}
+            target="_blank"
+          >
+            React Quiz App
+          </a>{" "}
+          - Simple Quiz App made with ReactJS{" "}
+          <span className={styles.language}>(ReactJS, TypeScript)</span>
+        </p>
+        <p>
+          <a
+            href="https://github.com/Kubajsa/jacobdev.tk"
+            className={styles.project}
+            target="_blank"
+          >
+            jacobdev.tk
+          </a>{" "}
+          - My First Website <span className={styles.language}>(NextJS)</span>
+        </p>
+        <p>
+          <a
+            href="https://github.com/Kubajsa/discord.pyBot"
+            className={styles.project}
+            target="_blank"
+          >
+            Kubajsa's Bot
+          </a>{" "}
+          - Discord bot made with discord.py{" "}
+          <span className={styles.language}>(Python)</span>
+        </p>
+        <a href="https://jacobdev.tk" className={styles.homeButton}>
+          home
+        </a>
+      </div>
     </div>
   );
 }
